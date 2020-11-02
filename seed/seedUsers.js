730f90f84c786d9d2054bc0a150df2defc92261b
PrimeWalkerVN <nchithanh609@gmail.com>
2020-11-02 16:01:11 +0700

    refactor

--- seed/seedUsers.js
+++ seed/seedUsers.js
@@ -1,29 +1,13 @@
 const seeder = require('mongoose-seed');
 const faker = require('faker');
 
-const unique = [
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-  '14',
-  '15',
-];
 let items = [];
 for (i = 0; i < 15; i++) {
   items.push({
-    username: faker.internet.userName() + unique[i],
+    username: faker.internet.userName() + i,
     password: '123456',
     email: faker.internet.email(),
+    fullName: faker.internet.userName(),
   });
 }
 
